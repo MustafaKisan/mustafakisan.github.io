557f0ca090e590f3ae8ed04e04e93c4a6bffd41e
MustafaKisan <20232538@std.neu.edu.tr>
2026-06-03 22:36:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website successfully loaded for Near East University SWE project.");
+    console.log("Developer: Mustafa Kisan (20232538)");
+});
